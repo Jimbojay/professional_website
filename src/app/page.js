b74b640ab7027e8ae535094b1d66b4a39c8a09df
jimbojay <jimbojay@LAPTOP-ERCJ5R8G>
2024-10-21 14:33:20 +0200

    only horizontal logos

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,6 @@
-"use client"; // Mark this as a Client Component
-
-import { useEffect, useState } from 'react';
+// src/app/page.js
 
 export default function Home() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY > 300) { // Adjust the scroll threshold to your needs
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
   return (
     <div>
       {/* Full-width banner */}
@@ -44,7 +23,7 @@ export default function Home() {
 
       {/* Logo section below the banner */}
       <section className="logo-section">
-        <div className={`logo-container ${isScrolled ? 'circle' : ''}`}>
+        <div className="logo-container">
           <img src="/assets/images/Data Management.png" alt="Logo 1" />
           <img src="/assets/images/Data Governance.png" alt="Logo 2" />
           <img src="/assets/images/Project Management.png" alt="Logo 3" />
@@ -52,20 +31,7 @@ export default function Home() {
           <img src="/assets/images/Business Intelligence.png" alt="Logo 5" />
           <img src="/assets/images/ProductDataExpert.png" alt="Logo 6" />
         </div>
-        
-        {/* Venn diagram image, visible only in the scrolled state */}
-        {isScrolled && (
-          <img
-            src="/assets/images/VennDiagram.png"
-            alt="Venn Diagram"
-            className="venn-diagram"
-          />
-        )}
       </section>
-
-
-      {/* Spacer to add 1500px of whitespace at the bottom */}
-      <div style={{ height: '1500px' }}></div>
     </div>
   );
 }
